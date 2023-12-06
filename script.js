0511ec75cf4c6133e3e2c5a2cf233c09266f72c2
Javeria <49153058+JaveriaHamid@users.noreply.github.com>
2023-12-06 14:06:43 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
 const cells = document.querySelectorAll(".cell");
 const turnText = document.querySelector(".turn");
 const restartBtn = document.querySelector(".reset");
-const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+
 let currentPlayer = "X";
+let nextPlayer = "O";
+let playerTurn = currentPlayer;
 
 
-playGame();
 
-function playGame(){
-    cells.forEach(cell => cell.addEventListener('click', cellClicked));
+// Function to start game
+const playGame = () => {
+    cells.forEach(cell =>{cell.addEventListener("click", cellClicked)});
     restartBtn.addEventListener('click', restartGame);
 }
 
-// function for change turn
+
+// Function to change turn
 const changeTurn = () => {
-    return currentPlayer === "X" ? "O" : "X";
-}
-
-function cellClicked(e) {
-    const cell = e.target;
-    if (cell.textContent === "") {
-        cell.textContent = currentPlayer;
-        currentPlayer = changeTurn();
-        turnText.textContent = `${currentPlayer}'s Turn`;
-        if(checkWinner()){
-           return;
-        }        
-        else if(draw()){
-            turnText.textContent = "Tie";
-        }
-    }
+    playerTurn = playerTurn === currentPlayer ? nextPlayer : currentPlayer;
 }
 
 
-// check win conditions & winner
-function checkWinner() {
-    let roundWon = false;
+// Function to check winner
+const checkWinner = () => {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     for (let i = 0; i < winConditions.length; i++) {
         const [cellA, cellB, cellC] = winConditions[i];
 
-        if (cells[cellA].textContent !== '' &&
-            cells[cellA].textContent === cells[cellB].textContent &&
-            cells[cellB].textContent === cells[cellC].textContent) {
-            roundWon = true;
+        if(cells[cellA].textContent !== '' &&
+           cells[cellA].textContent === cells[cellB].textContent &&
+           cells[cellB].textContent === cells[cellC].textContent){
             removeListener();
             turnText.textContent = cells[cellA].textContent + " Won";
             cells[cellA].style.backgroundColor = "#837b7b";
             cells[cellB].style.backgroundColor = "#837b7b";
             cells[cellC].style.backgroundColor = "#837b7b";
             document.querySelector('.gif-box').getElementsByTagName('img')[0].style.width = '123px';
-            break;
         }
     }
 }
 
-//  Game Draw
-function draw(){
+
+const cellClicked = (e) => {
+    if (e.target.textContent === "") {
+        e.target.textContent = playerTurn;
+        changeTurn();
+        turnText.textContent = `${playerTurn}'s Turn`;
+        if(checkWinner()){
+           return true;
+        }
+        else if(draw()){
+            removeListener();
+           turnText.textContent = "Tie";
+        }
+   }
+}
+
+
+// Function to check draw
+const draw = () => {
     let emptyCells = 0;
     cells.forEach(cell => {
         if(cell.textContent === ""){
@@ -74,12 +78,18 @@ function draw(){
     return emptyCells === 0 && !checkWinner();
 }
 
-// remove EventListener
-function removeListener() {
+
+// remove Event Listener
+const removeListener = () => {
     cells.forEach(cell => cell.removeEventListener('click', cellClicked));
 }
 
+
 // restart game
-function restartGame() {
+const restartGame = () => {
     return window.location.reload();
 }
+
+
+// Calling play game function
+playGame();
